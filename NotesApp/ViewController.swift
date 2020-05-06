e0357aa910d8f27a975217ca3ec14374a51fc7d0
Ky Nguyen <kynguyen@Kys-MacBook-Pro.local>
2020-05-06 13:04:34 +0700

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Ky Nguyen on 5/6/20.
+//  Copyright © 2020 Ky Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
